2783c25205f9850d136162f49d6239f1eba03afe
ocornut <omar@miracleworld.net>
2025-04-17 16:53:12 +0200

    Fonts: ImFontAtlasBuildInit() is always called with atlas->Builder == NULL.

--- imgui_internal.h
+++ imgui_internal.h
@@ -4055,7 +4055,7 @@ struct ImFontAtlasBuilder
 
     // Cache of all ImFontBaked
     ImStableVector<ImFontBaked,32> BakedPool;
-    ImGuiStorage                BakedMap;
+    ImGuiStorage                BakedMap;               // BakedId --> ImFontBaked*
     int                         BakedDiscardedCount;
 
     // Custom rectangle identifiers
